3ca418956b6246b021e114f166b30b8714d118af
Marcus Grainger <mgrainger@uvic.ca>
2024-11-05 10:34:05 -0800

    Add user type info

--- backend/login.py
+++ backend/login.py
@@ -2,7 +2,7 @@ import json
 
 def loadJSON():
     # todo - don't hardcode to a single DB name
-    db_fp = open("db.json")
+    db_fp = open("login.json")
     db = json.load(db_fp)
     db_fp.close()
     return db
@@ -10,16 +10,16 @@ def loadJSON():
 def checkLogin(db, username, password):
     """
     Given database, username and password,
-    returns True if password is correct, and False otherwise
+    returning a string based on login status (Admin, Medical, User, Fail)
     """
     for entry in db:
         if username not in entry.keys():
             continue
         elif entry[username] != password:
-            return False
+            return "Fail"
         else:
-            return True
-    return False
+            return entry["Type"]
+    return "Fail"
 
 db = loadJSON()
 print(db)  # For test purposes only
@@ -30,7 +30,7 @@ password = input("Enter a password: ")
 
 logIn = checkLogin(db, username, password)
 
-if logIn == True:
-    print("Logged in")
+if logIn != "Fail":
+    print("Logged in as " + logIn)
 else:
     print("Incorrect login info")
